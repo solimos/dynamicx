325535731fdd94603f2d62f77bd90acf668848d2
solimos <bryta-idolen-6k@icloud.com>
2025-01-18 00:22:23 +0100

    w

---
+++ script.js
@@ -0,0 +1,3 @@
+// 1. Move your mouse across the core to influence the shader's patterns and colors.
+// 2. Click and drag to change the camera view and explore the scene from different angles.
+// 3. Scroll to zoom in and out, revealing more details of the rings and core.
